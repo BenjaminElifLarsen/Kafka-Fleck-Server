514579822139e94ff98ad37aab08d1505a5325c6
Beela <bla@senia.dk>
2022-05-11 14:34:16 +0200

    start

---
+++ Kafka-Fleck-Server/Kafka-Fleck-Server/Consumer.cs
@@ -0,0 +1,54 @@
+﻿using Confluent.Kafka;
+using Fleck;
+
+namespace Kafka_Fleck_Server;
+
+internal class Consumer
+{
+    private readonly string bsServer = "172.16.250.13:9092";
+    private const string _topic = "house";
+    private readonly ConsumerConfig _config;
+    private readonly CancellationTokenSource _cts = new();
+
+    public Consumer()
+    {
+        Console.CancelKeyPress += (_, e) => {
+            e.Cancel = true;
+            _cts.Cancel();
+        };
+        _config = new()
+        {
+            GroupId = Guid.NewGuid().ToString(),
+            BootstrapServers = bsServer,
+            AutoOffsetReset = AutoOffsetReset.Latest
+        };
+    }
+
+    public void Consume(IList<IWebSocketConnection> allSockets)
+    {
+        using (var consumer = new ConsumerBuilder<Null, string>(_config).Build())
+        {
+            consumer.Subscribe(_topic);
+            try
+            {
+                while (true)
+                {
+                    var cr = consumer.Consume(_cts.Token);
+                    var data = cr.Message.Value;
+                    foreach(var socket in allSockets)
+                    {
+                        socket.Send(data);
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Ctrl-C was pressed.
+            }
+            finally
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ Kafka-Fleck-Server/Kafka-Fleck-Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using Fleck;
+using Kafka_Fleck_Server;
+//https://github.com/statianzo/Fleck/blob/master/src/Samples/ConsoleApp/Server.cs
+FleckLog.Level = LogLevel.Debug;
+var allSockets = new List<IWebSocketConnection>();
+var server = new WebSocketServer("ws://0.0.0.0:8181");
+server.Start(socket =>
+{
+    socket.OnOpen = () =>
+    {
+        Console.WriteLine("Open!");
+        allSockets.Add(socket);
+    };
+    socket.OnClose = () =>
+    {
+        Console.WriteLine("Close!");
+        allSockets.Remove(socket);
+    };
+    socket.OnMessage = message =>
+    {
+        Console.WriteLine(message);
+        allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+    };
+});
+
+new Consumer().Consume(allSockets);
+var input = Console.ReadLine();
+while (input != "exit")
+{
+    foreach (var socket in allSockets.ToList())
+    {
+        socket.Send(input);
+    }
+    input = Console.ReadLine();
+}
